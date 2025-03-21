58390d6ffac7e5fa49e629c291b961a25961ea71
husain-khan <136927986+husain-khan@users.noreply.github.com>
2025-03-21 19:27:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (89.42%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            
+            // Handle duplicates: left and mid are the same
+            if (nums[left] == nums[mid]) {
+                left++;
+                continue;
+            }
+            
+            // Check which part is sorted
+            if (nums[left] <= nums[mid]) { // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid ;
+                }
+            }
+        }
+        return false;
+    }
+};
