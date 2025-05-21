ba4cbb4c33db0076901391c7bf304a2d72d834f7
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-05-21 12:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (52.14%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<bool> row(m, false);  // Track rows to be zeroed
+        vector<bool> col(n, false);  // Track columns to be zeroed
+
+        // First pass: Mark rows and columns that need to be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Second pass: Set the marked rows and columns to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
