567a9d2ebbe9b548ac47c1d9a095350f41e58f02
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-05-25 14:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (38.86%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,34 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
- public:
-  ListNode* reverseKGroup(ListNode* head, int k) {
-    if (head == nullptr)
-      return nullptr;
+public:
+    ListNode* reversesub(ListNode* H, ListNode* T) {
+        ListNode* curr = H;
+        ListNode* prev = nullptr;
+        ListNode* next = nullptr;
 
-    ListNode* tail = head;
+        while (curr != T) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
 
-    for (int i = 0; i < k; ++i) {
-      // There are less than k nodes in the list, do nothing.
-      if (tail == nullptr)
-        return head;
-      tail = tail->next;
+        return prev; // New head of reversed part
     }
 
-    ListNode* newHead = reverse(head, tail);
-    head->next = reverseKGroup(tail, k);
-    return newHead;
-  }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
 
- private:
-  // Reverses [head, tail).
-  ListNode* reverse(ListNode* head, ListNode* tail) {
-    ListNode* prev = nullptr;
-    ListNode* curr = head;
-    while (curr != tail) {
-      ListNode* next = curr->next;
-      curr->next = prev;
-      prev = curr;
-      curr = next;
+        ListNode dummy(0); // Dummy node before head
+        dummy.next = head;
+        ListNode* prevGroupTail = &dummy;
+        ListNode* Head = head;
+
+        while (true) {
+            ListNode* Tail = Head;
+
+            // Check if there are at least k nodes ahead
+            for (int i = 0; i < k; ++i) {
+                if (!Tail) return dummy.next; // Not enough nodes
+                Tail = Tail->next;
+            }
+
+            // Reverse sublist from Head to Tail (exclusive)
+            ListNode* reversedHead = reversesub(Head, Tail);
+
+            // Link previous group's tail to new head
+            prevGroupTail->next = reversedHead;
+
+            // Head now becomes tail of the reversed sublist
+            Head->next = Tail;
+
+            // Update for next iteration
+            prevGroupTail = Head;
+            Head = Tail;
+        }
+
+        return dummy.next;
     }
-    return prev;
-  }
 };
