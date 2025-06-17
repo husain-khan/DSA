4de0d52ea5066a2b84c262635aaf9e3cc7d42710
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-06-17 16:55:36 +0530

    Time: 663 ms (5.05%) | Memory: 85.8 MB (37.29%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,20 +1,54 @@
 class Solution {
- public:
-  int largestRectangleArea(vector<int>& heights) {
-    int ans = 0;
-    stack<int> stack;
+public:
+   vector<int> PreviousSmallerElement(vector<int>& arr,int size){
+    stack<int> st;
+    vector<int> result(size);
+           for(int i=0;i<size;i++){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
 
-    for (int i = 0; i <= heights.size(); ++i) {
-      while (!stack.empty() &&
-             (i == heights.size() || heights[stack.top()] > heights[i])) {
-        const int h = heights[stack.top()];
-        stack.pop();
-        const int w = stack.empty() ? i : i - stack.top() - 1;
-        ans = max(ans, h * w);
-      }
-      stack.push(i);
+            if(st.empty()){
+                result[i]=-1;
+                }
+            else{
+                result[i]=st.top();
+            }
+
+            st.push(i);
+           }
+           return result;
+   }
+    vector<int> NextSmallerElement(vector<int>& arr,int size){
+    stack<int> st;
+    vector<int> result(size);
+           for(int i=size-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                result[i]=size;
+                }
+            else{
+                result[i]=st.top();
+            }
+
+            st.push(i);
+           }
+           return result;
+   }
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+       vector<int>pse= PreviousSmallerElement(heights,n);
+       vector<int>nse= NextSmallerElement(heights,n);
+      int ans=0;
+    for(int i=0;i<n;i++){
+        cout<<nse[i];
     }
-
-    return ans;
-  }
+        for(int i=0;i<=n-1;i++){
+            ans=max(ans,heights[i]*(nse[i]-pse[i]-1));
+        }
+        return ans;
+    }
 };
