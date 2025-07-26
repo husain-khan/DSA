1e236b279e8c336e39438754b26a3d416e9ce1f2
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-07-26 10:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (31.25%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,29 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    
-    vector<int> inorderTraversal(TreeNode* node) {
-     vector<int> ans;
-     traversal(node,ans);
-     return ans;
-    }
-    void traversal(TreeNode* node, vector<int>&ans){
-           if(node==NULL){
-            return;
-        }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
         
-        traversal(node->left,ans);
-        ans.push_back(node->val);
-        traversal(node->right,ans);
+        while (curr != nullptr || !st.empty()) {
+            // Go to the leftmost node
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            // Process the node
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+
+            // Visit the right subtree
+            curr = curr->right;
+        }
+
+        return ans;
     }
 };
