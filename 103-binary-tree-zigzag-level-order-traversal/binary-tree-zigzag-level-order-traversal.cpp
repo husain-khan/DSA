535bffbb56c647a7b318cede7400c004e28ed338
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-07-29 11:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (5.90%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -1,38 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == nullptr) return ans;
-
         queue<TreeNode*> q;
         q.push(root);
-        solve(root, q, ans);
+        vector<vector<int>> ans;
+        solve(root,q,ans);
         return ans; 
     }
 
-    vector<vector<int>> solve(TreeNode* node, queue<TreeNode*>& q, vector<vector<int>>& ans) {
-        bool flag = false;
-
-        while (!q.empty()) {
-            int levelSize = q.size(); // Important: fix size before the loop
-            vector<int> temp;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* curr = q.front(); 
+    vector<vector<int>> solve(TreeNode* node,queue<TreeNode*> &q,vector<vector<int>>&ans){
+        if(node==nullptr){
+            return ans;
+        }
+        bool flag=false;
+        
+        while(!q.empty()){
+           
+            vector<int>temp;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode *curr=q.front(); 
                 q.pop();
                 temp.push_back(curr->val);
-                if (curr->left != nullptr) q.push(curr->left);
-                if (curr->right != nullptr) q.push(curr->right);
+                if(curr->left!=nullptr)q.push(curr->left);
+                if(curr->right!=nullptr)q.push(curr->right);
             }
-
-            if (flag) {
+             if (flag) {
                 reverse(temp.begin(), temp.end());
             }
-
             ans.push_back(temp);
-            flag = !flag; // flip after each level
+            flag = !flag;
         }
-
         return ans;
     }
 };
