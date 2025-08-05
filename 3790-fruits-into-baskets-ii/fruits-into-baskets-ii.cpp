d2d9f3415b16929ab947ad9f28266add10b0734e
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-08-05 11:15:08 +0530

    Time: 5 ms (23.62%) | Memory: 49 MB (49.69%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int fsize=fruits.size();
-        int bsize=baskets.size();
-        int count=0;
-        vector<bool>booli(bsize,true);
-        for(int i=0;i<fsize;i++){
-            for(int j=0;j<bsize;j++){
-                if(fruits[i]<=baskets[j] && booli[j]==true){
+        int fsize = fruits.size();
+        int bsize = baskets.size();              // Fix here
+
+        int count = 0;
+        vector<bool> booli(bsize, true);         // Fix here
+
+        for (int i = 0; i < fsize; i++) {
+            for (int j = 0; j < bsize; j++) {
+                if (fruits[i] <= baskets[j] && booli[j]) {
                     count++;
-                    booli[j]=false;
+                    booli[j] = false;
                     break;
                 }
             }
         }
-        cout<<count;
-        return fsize-count;
+        
+        cout << count << endl;
+        return fsize - count;
     }
 };
