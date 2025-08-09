9e8ed1de34323bfd087932a1e527d8ddabc5244a
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-08-09 18:35:48 +0530

    Time: 7 ms (11.47%) | Memory: 27.9 MB (98.73%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,18 +1,28 @@
+#include<bits/stdc++.h>
+#include <algorithm> 
+using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
         int n=nums.size();
-        int count=n/2;
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            int countinhash=mp[nums[i]];
-            if(countinhash>count){
-                ans=max(ans,nums[i]);
+         if (n == 0) return -1;
+        std::sort(nums.begin(), nums.end());
+        
+        int maxelement =nums[0];
+        int count=1;
+        int maxcount=1;
+        for(int i=0;i<nums.size()-1;i++){
+           if (nums[i] == nums[i + 1]) {
+                count++;
+            } else {
+                count = 1;
             }
-              
+
+            if (count > maxcount) {
+                maxcount = count;
+                maxelement = nums[i];
+            }
         }
-        return ans;
+        return maxelement;
     }
 };
