c7203e4150149beeb1fc567d7935d64356917ed3
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-08-10 14:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (79.60%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,30 +1,37 @@
 class Solution {
- public:
-  vector<vector<int>> verticalTraversal(TreeNode* root) {
-    vector<vector<int>> ans;
-    map<int, multiset<pair<int, int>>> xToSortedPairs;  // {x: {(-y, val)}}
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        // col -> (row -> multiset of node values)
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> Q; // node, {row, col}
 
-    dfs(root, 0, 0, xToSortedPairs);
+        Q.push({root, {0, 0}});
 
-    for (const auto& [_, pairs] : xToSortedPairs) {
-      vector<int> vals;
-      for (const pair<int, int>& pair : pairs)
-        vals.push_back(pair.second);
-      ans.push_back(vals);
+        while (!Q.empty()) {
+            auto [node, pos] = Q.front();
+            Q.pop();
+            int row = pos.first;
+            int col = pos.second;
+
+            // Store value in map
+            nodes[col][row].insert(node->val);
+
+            // Push children with updated row/col
+            if (node->left)
+                Q.push({node->left, {row + 1, col - 1}});
+            if (node->right)
+                Q.push({node->right, {row + 1, col + 1}});
+        }
+
+        // Now build output from sorted map
+        vector<vector<int>> ans;
+        for (auto &colPair : nodes) {
+            vector<int> colVals;
+            for (auto &rowPair : colPair.second) {
+                colVals.insert(colVals.end(), rowPair.second.begin(), rowPair.second.end());
+            }
+            ans.push_back(colVals);
+        }
+        return ans;
     }
-
-    return ans;
-  }
-
- private:
-  void dfs(TreeNode* root, int x, int y,
-           map<int, multiset<pair<int, int>>>& xToSortedPairs) {
-    if (root == nullptr)
-      return;
-    xToSortedPairs[x].emplace(y, root->val);
-    dfs(root->left, x - 1, y + 1, xToSortedPairs);
-    dfs(root->right, x + 1, y + 1, xToSortedPairs);
-  }
 };
-
-
