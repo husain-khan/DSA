2486029e1d6ef21880c757d01e4739021622877c
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-08-28 00:53:42 +0530

    Time: 104 ms (5.01%) | Memory: 23.7 MB (5.98%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,22 +1,27 @@
 class Solution {
- public:
-  int candy(vector<int>& ratings) {
-    const int n = ratings.size();
-    int ans = 0;
-    vector<int> l(n, 1);
-    vector<int> r(n, 1);
-
-    for (int i = 1; i < n; ++i)
-      if (ratings[i] > ratings[i - 1])
-        l[i] = l[i - 1] + 1;
-
-    for (int i = n - 2; i >= 0; --i)
-      if (ratings[i] > ratings[i + 1])
-        r[i] = r[i + 1] + 1;
-
-    for (int i = 0; i < n; ++i)
-      ans += max(l[i], r[i]);
-
-    return ans;
-  }
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> lr(n,1);
+        vector<int> rl(n,1);
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                lr[i]=lr[i-1]+1;
+            }
+        }
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i]){
+               rl[i-1]=rl[i]+1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=max(lr[i],rl[i]);
+            // cout<<sum;
+            cout<<lr[i];
+            ans=ans+sum;
+        }
+        return ans;
+    }
 };
