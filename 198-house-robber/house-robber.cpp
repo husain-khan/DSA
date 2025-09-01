5a9efe5d2c2cab88671bcc39fadc979c3d24ff9e
Mohd Husain Khan <136927986+husain-khan@users.noreply.github.com>
2025-09-01 10:32:05 +0530

    Time: 3 ms (5.77%) | Memory: 13.1 MB (5.85%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int idx=0;
-        int sum=0;
+        int i=0;
         vector<int>dp(n,-1);
-        int ans=INT_MIN;
-       return helper(idx,nums,n,dp);
+        int sum=0;
+       return helper(i,n,nums,dp,sum);
+        
     }
 
-    int helper(int i,vector<int>&nums,int n,vector<int>&dp){
-        if(i>=n){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-
-       int take= helper(i+2,nums,n,dp)+nums[i];
-       int notake= helper(i+1,nums,n,dp);
-       dp[i]=max(take,notake);
+    int helper(int i,int n,vector<int>nums,vector<int>&dp,int sum){
+       if(i>=n){
+        return sum;
+       }
+       if(dp[i]!=-1){
         return dp[i];
+       }
+       int choose=helper(i+2,n,nums,dp,sum)+nums[i];
+
+       int notchoose=helper(i+1,n,nums,dp,sum);
+       dp[i]=max(choose,notchoose);
+       return dp[i];
     }
 };
